727836092ff0558a2c48476d041bb39a4a57a1a7
SrGodoy <luccasgodoy2012@gmail.com>
2025-07-17 20:29:40 -0300

    Teste de comentario

--- src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CadastroDeNinjasApplication {
-
+//Teste
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroDeNinjasApplication.class, args);
 	}
