502966c77d4e50511529b92cdaf6b52dd1c41042
SrGodoy <luccasgodoy2012@gmail.com>
2025-07-19 22:31:21 -0300

    Modelagem do Banco de Dados, NinjaModel e MissoesModel Concluidos, anotations feitas para relacionamento de tabelas

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,36 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+
+//argumentos
+@Entity
+@Table (name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String dificuldade;
+
+    //@OneToMany - Uma missao pode ter varios ninjas 1 para N (conceito de entidade relacionamento, relacionamento de tabelas de banco de dados)
+    @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
+    private List<NinjaModel> ninjas;
+
+}
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,6 +1,9 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 // Entity ele tranforma uma classe em uma entidade do BD
 // JPA = Java Persistence API
@@ -10,10 +13,16 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String nome;
-    String email;
-    int idade;
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+
+    //@ManyToOne um ninja tem uma unica missao
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") // cria a foreign key na tabela tb_cadastro
+    private MissoesModel missoes;
 
     public NinjaModel() {
     }
