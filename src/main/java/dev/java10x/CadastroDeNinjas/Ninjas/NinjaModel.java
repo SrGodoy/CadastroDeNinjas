874c6860c23d4d6094e0be2d604c636866d6622c
SrGodoy <luccasgodoy2012@gmail.com>
2025-07-19 22:41:11 -0300

    adcionando Longbok em NinjasModel e MissoesModel

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,6 +2,9 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -9,51 +12,25 @@ import java.util.List;
 // JPA = Java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
-    private String email;
-    private int idade;
 
+    private String nome;
+
+    private String email;
+
+    private int idade;
 
     //@ManyToOne um ninja tem uma unica missao
     @ManyToOne
     @JoinColumn(name = "missoes_id") // cria a foreign key na tabela tb_cadastro
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
 
-    public NinjaModel(String email, int idade, String nome) {
-        this.email = email;
-        this.idade = idade;
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
